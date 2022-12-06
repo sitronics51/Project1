45011f72f10c16556669cc2a6e9e22f1adea4911
Gheorghe Siminițchi <34488513+sitronics51@users.noreply.github.com>
2022-12-06 12:35:42 +0200

    Commit in Project1 - HelloWorld

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -1,2 +1,18 @@
-package HelloWorld;public class HelloWorld {
+package HelloWorld;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        /*String name = "Jony!";
+        System.out.println("Hello, " + name);// Metoda de citire dintr-o variabilДѓ
+        */
+        Scanner scanner1 = new Scanner(System.in);
+        String name;
+
+        System.out.println("Enter your first and last name:");
+        name = scanner1.nextLine();
+        System.out.print("Hello, " + name);
+    }
 }
+
